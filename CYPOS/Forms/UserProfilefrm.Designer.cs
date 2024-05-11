2ba5257e6d74cf1062e0182535fabee602cab481
nadayassin19 <yassinada19@gmail.com>
2024-05-11 03:51:59 +0300

    UserProfilefrm edit
    Due to design loss every time I close visual studio or start to code on the form, I'm keeping pushing every single detail whenever it's done, to prevent work loss.

--- CYPOS/Forms/UserProfilefrm.Designer.cs
+++ CYPOS/Forms/UserProfilefrm.Designer.cs
@@ -33,6 +33,17 @@
             this.txtUserFullName = new System.Windows.Forms.TextBox();
             this.btnKbName = new System.Windows.Forms.Button();
             this.PicUserPhoto = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnKbAddress = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnKbEmail = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.btnKbContact = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtDob = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.PicUserPhoto)).BeginInit();
             this.SuspendLayout();
             // 
@@ -79,11 +90,142 @@
             this.PicUserPhoto.TabIndex = 0;
             this.PicUserPhoto.TabStop = false;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(240, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Address";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtAddress.Location = new System.Drawing.Point(240, 100);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtAddress.Size = new System.Drawing.Size(270, 95);
+            this.txtAddress.TabIndex = 5;
+            // 
+            // btnKbAddress
+            // 
+            this.btnKbAddress.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKbAddress.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnKbAddress.Image = ((System.Drawing.Image)(resources.GetObject("btnKbAddress.Image")));
+            this.btnKbAddress.Location = new System.Drawing.Point(530, 100);
+            this.btnKbAddress.Name = "btnKbAddress";
+            this.btnKbAddress.Size = new System.Drawing.Size(40, 30);
+            this.btnKbAddress.TabIndex = 6;
+            this.btnKbAddress.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(240, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtEmail.Location = new System.Drawing.Point(240, 220);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtEmail.Size = new System.Drawing.Size(270, 30);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // btnKbEmail
+            // 
+            this.btnKbEmail.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKbEmail.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnKbEmail.Image = ((System.Drawing.Image)(resources.GetObject("btnKbEmail.Image")));
+            this.btnKbEmail.Location = new System.Drawing.Point(530, 220);
+            this.btnKbEmail.Name = "btnKbEmail";
+            this.btnKbEmail.Size = new System.Drawing.Size(40, 30);
+            this.btnKbEmail.TabIndex = 9;
+            this.btnKbEmail.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(240, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Contact";
+            // 
+            // txtContact
+            // 
+            this.txtContact.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContact.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtContact.Location = new System.Drawing.Point(240, 275);
+            this.txtContact.Multiline = true;
+            this.txtContact.Name = "txtContact";
+            this.txtContact.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtContact.Size = new System.Drawing.Size(270, 30);
+            this.txtContact.TabIndex = 11;
+            // 
+            // btnKbContact
+            // 
+            this.btnKbContact.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKbContact.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnKbContact.Image = ((System.Drawing.Image)(resources.GetObject("btnKbContact.Image")));
+            this.btnKbContact.Location = new System.Drawing.Point(530, 275);
+            this.btnKbContact.Name = "btnKbContact";
+            this.btnKbContact.Size = new System.Drawing.Size(40, 30);
+            this.btnKbContact.TabIndex = 12;
+            this.btnKbContact.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(240, 315);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 18);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Date Of Birth";
+            // 
+            // dtDob
+            // 
+            this.dtDob.CustomFormat = "31 - Dec - 2010";
+            this.dtDob.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtDob.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtDob.Location = new System.Drawing.Point(340, 315);
+            this.dtDob.Name = "dtDob";
+            this.dtDob.Size = new System.Drawing.Size(168, 26);
+            this.dtDob.TabIndex = 14;
+            // 
             // UserProfilefrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(593, 392);
+            this.Controls.Add(this.dtDob);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnKbContact);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnKbEmail);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnKbAddress);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnKbName);
             this.Controls.Add(this.txtUserFullName);
             this.Controls.Add(this.label1);
@@ -102,5 +244,16 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtUserFullName;
         private System.Windows.Forms.Button btnKbName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnKbAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnKbEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.Button btnKbContact;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtDob;
     }
 }
